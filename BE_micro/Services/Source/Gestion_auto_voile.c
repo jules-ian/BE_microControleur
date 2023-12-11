986bc1b96327753215a752bd4d2b4702703f43c3
yuZhang <122043326+yzhan2911@users.noreply.github.com>
2023-12-11 15:16:34 +0100

    Update Gestion_auto_voile.c

--- BE_micro/Services/Source/Gestion_auto_voile.c
+++ BE_micro/Services/Source/Gestion_auto_voile.c
@@ -1,5 +1,5 @@
 #include "Gestion_auto_voile.h"
-
+int cyclique;
 void init_girouette(GPIO_TypeDef * GPIO1, char GRPIO_Pin3){
 while((GPIO1->IDR&0x1<<GRPIO_Pin3)==0){}; //quand girouette est à la position init, on commence
 	TIM3->CNT=0;
@@ -37,32 +37,32 @@ void gestion_init_voile(TIM_TypeDef * Timer,GPIO_TypeDef * GPIO, char GRPIO_Pin)
 int calcul_rapport_cyclique(int angle_vente){
 	//sif (angle_vente>45&&angle)
 	int Duty_Cycle;
-	if (angle_vente<=35){
+	if (angle_vente<=30){
 			Duty_Cycle=100;
-	}else if(angle_vente>35&&angle_vente<=68){
+	}else if(angle_vente>30&&angle_vente<=60){
 			Duty_Cycle=90;
-	}else if(angle_vente>68&&angle_vente<=86){
+	}else if(angle_vente>60&&angle_vente<=90){
 			Duty_Cycle=80;
-	}else if(angle_vente>86&&angle_vente<=106){
+	}else if(angle_vente>90&&angle_vente<=120){
+			Duty_Cycle=70;
+	}else if(angle_vente>120&&angle_vente<=150){
 			Duty_Cycle=60;
-	}else if(angle_vente>106&&angle_vente<=143){
+	}else if(angle_vente>150&&angle_vente<210){
 			Duty_Cycle=50;
-	}else if(angle_vente>143&&angle_vente<223){
-			Duty_Cycle=50;
-	}else if(angle_vente>=223&&angle_vente<262){
-			Duty_Cycle=50;
-	}else if(angle_vente>=262&&angle_vente<279){
+	}else if(angle_vente>=210&&angle_vente<240){
 			Duty_Cycle=60;
-	}else if(angle_vente>=279&&angle_vente<305){
+	}else if(angle_vente>=240&&angle_vente<270){
+			Duty_Cycle=70;
+	}else if(angle_vente>=270&&angle_vente<300){
 			Duty_Cycle=80;
-	}else if(angle_vente>=305&&angle_vente<340){
+	}else if(angle_vente>=300&&angle_vente<330){
 			Duty_Cycle=90;
-	}else if(angle_vente>=340){
+	}else if(angle_vente>=330){
 			Duty_Cycle=100;}
 	return Duty_Cycle;
 }
 void gestion_mise_a_jour(TIM_TypeDef * Timer_girou, TIM_TypeDef * Timer_voile){
-		int cyclique;
+		
 		cyclique=calcul_rapport_cyclique(Get_angle_vent(Timer_girou));
 		PWM_SetCycle(Timer_voile,1,Timer_voile->ARR*cyclique/1000);
 		

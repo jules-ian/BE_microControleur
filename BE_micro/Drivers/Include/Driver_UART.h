8731b3fb3cadbd8678311922a49ec6138ad5cb59
jules-ian <115194459+jules-ian@users.noreply.github.com>
2023-12-11 16:23:20 +0100

    Added comments to Driver_UART.h

--- BE_micro/Drivers/Include/Driver_UART.h
+++ BE_micro/Drivers/Include/Driver_UART.h
@@ -5,9 +5,9 @@
 void MyUART_Init(char usart);
 
 
-signed char getValue1(void);
-signed char getValue2(void);
-signed char getValue3(void);
+signed char getValue1(void); //gets value of UART 1
+signed char getValue2(void); //gets value of UART 2
+signed char getValue3(void); //gets value of UART 3
 
 void MyUART_send(char* val, char usart);
 
